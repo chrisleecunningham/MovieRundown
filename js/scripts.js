71d722d108d72d31e3558bdecf40c0e84488ccca
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2022-06-28 16:40:30 -0700

    Update scripts.js
    added pokemon array

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
-let annesAge = 27;
-let anne = {
-  name: 'Anne',
-  age: annesAge
-  child: {
-    name: 'Joe',
-    age: 2
+let pokemonList = [
+  {name: 'Wigglytuff',
+  height: 1,
+  type: ['fairy', 'normal']
+  },
+  {name: 'Slowbro',
+  height: 1.6,
+  type: ['psychic', 'water']
+  },
+  {name: 'Mr. Mime',
+  height: 1.3,
+  type: ['psychic', 'fairy']
   }
-};
+];

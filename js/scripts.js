cd5d486526a130549b6636cc43f76bed17737a33
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2022-07-25 12:53:18 -0700

    Update scripts.js
    Cleaned up code and added list for array

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
 let pokemonList = [
-  {name: 'Wigglytuff',
-  height: 1,
-  type: ['fairy', 'normal']
+  {
+    name: 'Wigglytuff',
+    height: 1,
+    type: ['fairy', 'normal']
   },
-  {name: 'Slowbro',
-  height: 1.6,
-  type: ['psychic', 'water']
+  {
+    name: 'Slowbro',
+    height: 1.6,
+    type: ['psychic', 'water']
   },
-  {name: 'Mr. Mime',
-  height: 1.3,
-  type: ['psychic', 'fairy']
+  {
+    name: 'Mr. Mime',
+    height: 1.3,
+    type: ['psychic', 'fairy']
   }
 ];
+
+for (var i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.5) {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that\'s big!')
+  } else {
+      document.write('<p>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')</p>')
+  }
+}

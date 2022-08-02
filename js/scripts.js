25f07d7af0b008a8b89b99da792e5d7c3ab53adf
chrisleecunningham <103608696+chrisleecunningham@users.noreply.github.com>
2022-08-02 11:26:09 -0700

    Update scripts.js
    contain pokemonList within IIFE and update loop syntax to reflect

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
-let pokemonList = [
-  {
-    name: 'Wigglytuff',
-    height: 1,
-    type: ['fairy', 'normal']
-  },
-  {
-    name: 'Slowbro',
-    height: 1.6,
-    type: ['psychic', 'water']
-  },
-  {
-    name: 'Mr. Mime',
-    height: 1.3,
-    type: ['psychic', 'fairy']
+//IIFE containing pokemonList
+let pokemonRepository = (function (){
+  let pokemonList = [
+      {
+        name: 'Wigglytuff',
+        height: 1,
+        type: ['fairy', 'normal']
+      },
+      {
+        name: 'Slowbro',
+        height: 1.6,
+        type: ['psychic', 'water']
+      },
+      {
+        name: 'Mr. Mime',
+        height: 1.3,
+        type: ['psychic', 'fairy']
+      }
+    ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.add({name: 'Paras', height: .3, type:['grass', 'bug']});
 
 //forEach loop to list the pokemonList
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write(pokemon.name + ': height(' + pokemon.height + '), ' +
   pokemon.type[0] + '/' + pokemon.type[1] + '<br>')
 });
